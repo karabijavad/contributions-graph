ee4767415c10e904eaa0b7dc0663f74a61fb5833
Javad Karabi <karabijavad@gmail.com>
2014-03-26 10:03:45 -0500

    first

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
+rm -rf .bundle/
 mkdir .bundle/
 bundle install --path .bundle/
 
+rm -rf neo4j-community-2.0.1-unix
 curl -O http://dist.neo4j.org/neo4j-community-2.0.1-unix.tar.gz
 tar xf neo4j-community-2.0.1-unix.tar.gz
 rm neo4j-community-2.0.1-unix.tar.gz
 
-cd data/
-gzcat FEC_contributions.csv.gz > FEC_contributions.csv
+gzcat data/FEC_Contributions_2012.csv.gz > data/FEC_Contributions_2012.csv
