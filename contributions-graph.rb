e51bcfb2ba654a7b637c51437ec09830cc0d6682
Javad Karabi <karabijavad@gmail.com>
2014-03-26 21:56:48 -0500

    stuff

--- contributions-graph.rb
+++ contributions-graph.rb
@@ -5,27 +5,26 @@ require 'csv'
 
 Cadet::BatchInserter::Session.open "neo4j-community-2.0.1/data/graph.db" do
 
+  constraint :Contributor, :name
+  constraint :Employer,    :name
+  constraint :Occupation,  :name
+  constraint :Recipient,   :name
+
   CSV.foreach("./data/FEC_Contributions_2012.csv", :headers => :first_row) do |row|
-    next unless row["Recipient"] and row["Contributor/Lender/Transfer Name 1"] and row["Employer"] and row["Occupation"]
+    next unless row["Contributor/Lender/Transfer Name 1"]
 
     contributor = Contributor_by_name row["Contributor/Lender/Transfer Name 1"]
 
-    if row["Employer"]
-      employment = create_Employment({}).tap do |employment|
-        employment.employer_to   Employer_by_name(row["Employer"])
-        employment.occupation_to Occupation_by_name(row["Occupation"]) if row["Occupation"]
-      end
-    end
+    employment = create_Employment
+    employment.employer_to   Employer_by_name(row["Employer"]) if row["Employer"]
+    employment.occupation_to Occupation_by_name(row["Occupation"]) if row["Occupation"]
 
     contributor.employment_to employment
 
-    contribution = create_Contribution({}).tap do |contribution|
-      contribution.to_to Recipient_by_name(row["Recipient"])
+    contributor.contribution_to(Recipient_by_name(row["Recipient"])).tap do |contribution|
       contribution[:amount] = row["Amount"].gsub(/\$/, "").to_i
       contribution[:date]   = row["Transaction Date"].gsub(%r{(\d+)/(\d+)/(\d+)}, '\3\1\2').to_i
     end
-
-    contributor.contribution_to contribution
 
   end
 
